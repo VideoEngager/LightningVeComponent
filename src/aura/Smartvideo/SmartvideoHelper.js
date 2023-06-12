9929f2d781bade850eb4b93e844086f605fbbe16
Deso <desislav.michev@mtr-design.com>
2023-06-12 20:52:40 +0300

    add wrapper component for SmartvideoCore LWC

---
+++ src/aura/Smartvideo/SmartvideoHelper.js
@@ -0,0 +1,15 @@
+
+({
+    isUtilityBarApiMethod: function (methodName) {
+        // All methods and their parameters are available at
+        // https://developer.salesforce.com/docs/component-library/bundle/lightning:utilityBarAPI/specification
+        const availableMethods = [
+            'disableUtilityPopOut', 'getAllUtilityInfo', 'getEnclosingUtilityId', 'getUtilityInfo', 'isUtilityPoppedOut',
+            'minimizeUtility', 'onUtilityClick', 'openUtility', 'setPanelHeaderIcon', 'setPanelHeaderLabel',
+            'setPanelHeight', 'setPanelWidth', 'setUtilityHighlighted', 'setUtilityIcon', 'setUtilityLabel',
+            'toggleModalMode'
+        ];
+
+        return availableMethods.includes(methodName);
+    }
+});
